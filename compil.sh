a5e4d13bef7842ea309a24bc4dc78529b05949d9
David <davidletinaud@gmail.com>
2020-11-18 13:20:31 -0500

    script to compile the kernel and launch it on qemu

---
+++ compil.sh
@@ -0,0 +1,7 @@
+rm *.o
+rm myos.elf
+arm-none-eabi-gcc -mcpu=arm1176jzf-s -fpic -ffreestanding -c boot.S -o boot.o
+arm-none-eabi-gcc -mcpu=arm1176jzf-s -fpic -ffreestanding -std=gnu99 -c kernel.c -o kernel.o -O2 -Wall -Wextra
+arm-none-eabi-gcc -T linker.ld -o myos.elf -ffreestanding -O2 -nostdlib boot.o kernel.o
+
+qemu-system-arm -cpu arm1176 -m 256 -M raspi2 -serial stdio -kernel myos.elf
